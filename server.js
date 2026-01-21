2f9ba1170e902fe0bce43f2abea69e49d385029e
vairavelu1989-vel <vairavelu1989@gmail.com>
2026-01-21 14:51:25 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend working");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server started");
+});
