d97fdd8dac4bf2402e741036224fae6937c2558b
vairavelu1989-vel <vairavelu1989@gmail.com>
2026-01-22 12:59:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+let users = [];
+
 app.get("/", (req, res) => {
-  res.send("Backend working");
+  res.send("Backend is LIVE 🚀");
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Server started");
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+app.post("/users", (req, res) => {
+  const user = req.body;
+  users.push(user);
+  console.log("User added:", user);
+  res.json({ message: "User added", user });
+});
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
 });
